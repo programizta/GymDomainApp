2be6163fafd80548590843f96a623ea0eb3b22b0
Piotr Kołeczek <koleczkowsky@gmail.com>
2023-12-02 19:51:25 +0100

    Extracted non-endpoint specific logic to the ApiController base class

--- DomeGym.Api/Controllers/SubscriptionsController.cs
+++ DomeGym.Api/Controllers/SubscriptionsController.cs
@@ -3,16 +3,13 @@ using DomeGym.Application.Subscription.Commands.DeleteSubscription;
 using DomeGym.Application.Subscription.Queries.GetSubscription;
 using DomeGym.Contracts.Common;
 using DomeGym.Contracts.Subscriptions;
-using ErrorOr;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DomeGym.Api.Controllers;
 
-[ApiController]
 [Route("[controller]")]
-public class SubscriptionsController : ControllerBase
+public class SubscriptionsController : ApiController
 {
     private readonly IMediator _mediator; // or use ISender, because we implement only Send() method, so we could use smaller unit of interface => interface segregation principle
 
@@ -74,48 +71,5 @@ public class SubscriptionsController : ControllerBase
             Enum.Parse<SubscriptionType>(getSubscriptionResult.Value.SubscriptionDetails.SubscriptionName));
 
         return Ok(response);
-    }
-
-    public IActionResult Problem(List<Error> errors)
-    {
-        if (!errors.Any())
-        {
-            return Problem();
-        }
-
-        if (errors.All(x => x.Type == ErrorType.Validation))
-        {
-            return ValidationProblem(errors);
-        }
-
-        // for now, by default return first ocurred error
-        return Problem(errors[0]);
-    }
-
-    public IActionResult Problem(Error error)
-    {
-        var statusCode = error.Type switch
-        {
-            ErrorType.Conflict => StatusCodes.Status409Conflict,
-            ErrorType.Validation => StatusCodes.Status400BadRequest,
-            ErrorType.NotFound => StatusCodes.Status404NotFound,
-            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
-            ErrorType.Unexpected => StatusCodes.Status403Forbidden,
-            _ => StatusCodes.Status500InternalServerError
-        };
-
-        return Problem(statusCode: statusCode, detail: error.Description);
-    }
-
-    public IActionResult ValidationProblem(List<Error> errors)
-    {
-        var modelStateDictionary = new ModelStateDictionary();
-
-        foreach (var error in errors)
-        {
-            modelStateDictionary.AddModelError(error.Code, error.Description);
-        }
-
-        return ValidationProblem(modelStateDictionary);
     }
 }
